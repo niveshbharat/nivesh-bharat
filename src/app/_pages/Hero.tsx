4582cd1b214f2b42940dc9d29faf7d653999235d
Styles <bcs_2021035@iiitm.ac.in>
2024-02-12 23:10:14 +0530

    hero section fix

--- src/app/_pages/Hero.tsx
+++ src/app/_pages/Hero.tsx
@@ -4,7 +4,7 @@ import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
-    <div className="mb-20 xl:-mb-20 max-w-5xl px-4 pt-16 mx-10 sm:mt-14 xl:mx-auto">
+    <div className="mb-20 max-w-5xl px-4 pt-16 sm:mt-14 mx-auto">
       <div className="text-center">
         <h1 className="hidden md:block text-5xl font-bold font-lexend align-center text-center xl:text-7xl">
           <ReactTyped
@@ -20,7 +20,7 @@ const Hero = () => {
         <h1 className="md:hidden text-5xl font-bold font-lexend text-center">
           <p className="">Let&apos;s build your <span className="text-p_green-500">Wealth!</span></p>
         </h1>
-        <p className="text-xl text-body py-5">
+        <p className="text-xl text-body py-5 px-10">
           We build your Investment Portfolio by customizing your financial
           planning to match your goals and risk appetite. Introducing India’s
           First of its kind All-In-One Investment Platform to transform your
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,12 @@ import OurTeam from "@/components/OurTeam";
 export default function Home() {
   return (
     <main className="overflow-x-clip">
-      <section className="xl:h-screen">
+      <section className="">
         {/* Navbar */}
         <NavBar />
 
         {/* Hero */}
-        <section id="home" className="relative ">
+        <section id="home" className="relative xl:mb-20 flex flex-col justify-center">
           <div className="absolute blur-[600px] bg-p_orange/40 p-96 -z-10 -left-72 -top-56" />
           <div className="absolute blur-[600px] bg-p_green-500/40 p-96 -z-10 -right-72 -top-56 hidden md:block" />
           <Hero />
