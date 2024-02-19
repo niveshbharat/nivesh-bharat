f1d5b6f3cd2d3b36039a5d2c6f4dddfcde67b305
Styles <bcs_2021035@iiitm.ac.in>
2024-02-20 01:38:18 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
 
         {/* Hero */}
         <section id="home" className="relative xl:mb-20 flex flex-col justify-center">
-          <div className="absolute blur-[600px] bg-p_orange/40 p-96 -z-10 -left-72 -top-56" />
-          <div className="absolute blur-[600px] bg-p_green-500/40 p-96 -z-10 -right-72 -top-56" />
+          <div className="absolute blur-[600px] contrast-10 bg-p_orange/40 p-96 -z-10 -left-72 -top-56" />
+          <div className="absolute blur-[600px] contrast-10 bg-p_green-500/40 p-96 -z-10 -right-72 -top-56" />
           <Hero />
         </section>
       </section>
